b0cf2d539f50f191ed0372bee8094d018c07e3b8
ktndrnl <33733121+ktndrnl@users.noreply.github.com>
2019-04-21 18:04:33 -0500

    Update Program.cs

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -10,10 +10,176 @@ namespace ProjectEuler
 	{
 		static void Main(string[] args)
 		{
-			var problem12 = new Problem12();
-			problem12.Solution();
+//			var problem12 = new Problem12();
+//			problem12.Solution();
+            var problem11 = new Problem11();
+            problem11.Solution();
 		}
 	}
+
+    public class Problem11
+    {
+        private const string gridString = @"
+            08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+                49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+                81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+                52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+                22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+                24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+                32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+                67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+                24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+                21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+                78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+                16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+                86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+                19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+                04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+                88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+                04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+                20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+                20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+                01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
+
+        char[] seperators = new char[3];
+        private readonly string[] gridStrings;
+
+        private int gridDimension;
+        private int[][] intGrid;
+
+        int greatestProduct = 0;
+
+        public Problem11()
+        {
+            seperators[0] = ' ';
+            seperators[1] = '\n';
+            seperators[2] = '\r';
+            gridStrings = gridString.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+            gridDimension = (int) Math.Sqrt(gridStrings.Length);
+            intGrid = new int[gridDimension][];
+            for (int i = 0; i < gridDimension; i++)
+            {
+                intGrid[i] = new int[gridDimension];
+            }
+
+            int s = 0;
+            for (int i = 0; i < gridDimension; i++)
+            {
+                for (int j = 0; j < gridDimension; j++, s++)
+                {
+                    intGrid[i][j] = int.Parse(gridStrings[s]);
+                }
+            }
+        }
+
+        public void Solution()
+        {
+            FindGreatestVerticalProduct();
+            FindGreatestHorizontalProduct();
+            FindGreatestDiagonalDownProduct();
+            FindGreatestDiagonalUpProduct();
+            Console.WriteLine(greatestProduct);
+            Console.ReadKey();
+        }
+
+        public void FindGreatestVerticalProduct()
+        {
+            for (int i = 0; i < gridDimension; i++)
+            {
+                for (int j = 0; j < gridDimension; j++)
+                {
+                    if (i+3 < gridDimension)
+                    {
+                        int product = 0;
+                        product = intGrid[i][j] * intGrid[i + 1][j] * intGrid[i + 2][j] * intGrid[i + 3][j];
+                        if (product > greatestProduct)
+                        {
+                            greatestProduct = product;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void FindGreatestHorizontalProduct()
+        {
+            for (int i = 0; i < gridDimension; i++)
+            {
+                for (int j = 0; j < gridDimension; j++)
+                {
+                    if (j + 3 < gridDimension)
+                    {
+                        int product = 0;
+                        product = intGrid[i][j] * intGrid[i][j + 1] * intGrid[i][j + 2] * intGrid[i][j + 3]; 
+                        if (product > greatestProduct)
+                        {
+                            greatestProduct = product;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void FindGreatestDiagonalDownProduct()
+        {
+            for (int i = 0; i < gridDimension; i++)
+            {
+                for (int j = 0; j < gridDimension; j++)
+                {
+                    if ((j + 3 < gridDimension) && (i + 3 < gridDimension))
+                    {
+                        int product = 0;
+                        product = intGrid[i][j] *
+                                  intGrid[i + 1][j + 1] *
+                                  intGrid[i + 2][j + 2] *
+                                  intGrid[i + 3][j + 3];
+                        if (product > greatestProduct)
+                        {
+                            greatestProduct = product;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void FindGreatestDiagonalUpProduct()
+        {
+            for (int i = gridDimension - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < gridDimension; j++)
+                {
+                    if ((j + 3 < gridDimension) && (i - 3 >= 0))
+                    {
+                        int product = 0;
+                        product = intGrid[i][j] *
+                                  intGrid[i - 1][j + 1] *
+                                  intGrid[i - 2][j + 2] *
+                                  intGrid[i - 3][j + 3];
+                        if (product > greatestProduct)
+                        {
+                            greatestProduct = product;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
 	public class Problem12
 	{
@@ -28,7 +194,6 @@ namespace ProjectEuler
     	public void Solution(){
         	bool foundValue = false;
 			int i = 1;
-			DateTime startTime = DateTime.Now;
 			while(!foundValue){
 				int triangleNum = GetNthTriangleNum(i);
 				int numDivisors = GetTriangleNumDivisors(triangleNum).Count;
@@ -38,26 +203,22 @@ namespace ProjectEuler
 					Console.WriteLine($"{triangleNum} has {numDivisors} divisors.");
 					Console.WriteLine();
 					foundValue = true;
+					break;
 				}
 				else
 				{
-					if (DateTime.Now - startTime >= TimeSpan.FromSeconds(10))
-					{
-						Console.WriteLine($"{triangleNum} has {numDivisors} divisors.");
-						startTime = DateTime.Now;
-					}
 					i++;					
 				}
 			}
     	}
 
     	private int GetNthTriangleNum(int n){
-        	int triangleNum;
+        	int triangleNum = 0;
 			if (n <= 0)
 			{
 				return 0;
 			}
-			if (triangleNums.ContainsKey(n))
+			else if (triangleNums.ContainsKey(n))
 			{
 				return triangleNums[n];
 			}
@@ -70,13 +231,13 @@ namespace ProjectEuler
     	}
 
 		private List<int> GetTriangleNumDivisors(int triangleNum){
-			List<int> divisors = new List<int> {triangleNum};
-			for (int i = triangleNum / 2; i >= 1; i--){
+			List<int> divisors = new List<int>();
+			for (int i = triangleNum; i >= 1; i--){
 				if (triangleNum % i == 0)
 				{
 					if (triangleNumDivisors.ContainsKey(i))
 					{
-						divisors.AddRange(triangleNumDivisors[i]);
+						divisors.Concat(triangleNumDivisors[i]);
 						break;
 					}
 					divisors.Add(i);
@@ -86,4 +247,6 @@ namespace ProjectEuler
 			return divisors;
 		}
 	}
+
+
 }
